67d86f7b07f9cda855ed8e56d659e37655a69489
Abid191 <abid15-12563@diu.edu.bd>
2023-09-24 12:41:38 +0600

    seven

--- index.js
+++ index.js
@@ -125,6 +125,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/furniture/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await furnitureCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // cartCollection
 
